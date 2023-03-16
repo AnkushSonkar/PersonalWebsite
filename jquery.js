ab39ae388152a3e446e4a29bb5b70dc7461a3149
Ankush <ankushsonkar987@gmail.com>
2023-03-16 12:56:49 +0530

    initial commit

---
+++ jquery.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: "1",
+    loop: "true",
+    nav: "true",
+    dots: "true",
+    autoplay: "true",
+    autoplaySpeed: "3000",
+    smartspeed: "1500",
+    autoplayHoverPause: "true",
+  });
+
+  $("#home").click(function () {});
+});
